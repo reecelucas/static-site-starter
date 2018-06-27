28fa5406147773bb3daa700d84747f07fb9c14a8
Reece Lucas <reece.lucas@bbc.co.uk>
2018-06-27 15:03:26 +0100

    Formatting tweaks

--- src/js/utilities/validateEmail.js
+++ src/js/utilities/validateEmail.js
@@ -2,7 +2,7 @@
  * @param {String} email
  * @return {Boolean}
  */
-export default (email) => {
+export default email => {
     const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return emailRegex.test(email);
-}
+};
